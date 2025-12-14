b5c567c029327304043de354e333926e4d1201b5
Diegoagodoy <diegoadolfogodoy@hotmail.com>
2025-12-14 12:13:08 -0300

    Agregando telefono y e-mail al modelo Pacientes y actualizando el admin

--- Turnos/admin.py
+++ Turnos/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import Especialidad, Medico, Paciente, Turno
 
+class PacienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'dni', 'telefono', 'email')  # Mostrar campos en el admin
+
 admin.site.register(Especialidad)
 admin.site.register(Medico)
-admin.site.register(Paciente)
+admin.site.register(Paciente, PacienteAdmin)  # Añadir el Admin personalizado
 admin.site.register(Turno)
--- Turnos/models.py
+++ Turnos/models.py
@@ -21,7 +21,9 @@ class Medico(models.Model):
 class Paciente(models.Model):
     nombre = models.CharField(max_length=100)
     dni = models.CharField(max_length=20, unique=True)
-
+    telefono = models.CharField(max_length=15, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    
     def __str__(self):
         return self.nombre
 
